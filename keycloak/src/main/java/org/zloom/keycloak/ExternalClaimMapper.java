ca6f36a9a7972a970699c5357966007dc05bd093
Anton Zalialdinov <zloomsky@gmail.com>
2024-10-03 19:28:19 +0200

    refactor

---
+++ keycloak/src/main/java/org/zloom/keycloak/ExternalClaimMapper.java
@@ -0,0 +1,209 @@
+package org.zloom.keycloak;
+
+import com.google.auto.service.AutoService;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import org.apache.http.client.HttpResponseException;
+import org.keycloak.broker.provider.util.SimpleHttp;
+import org.keycloak.models.ClientSessionContext;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.UserSessionModel;
+import org.keycloak.protocol.ProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
+import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
+import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderConfigurationBuilder;
+import org.keycloak.representations.IDToken;
+import org.jboss.logging.Logger;
+import org.keycloak.util.JsonSerialization;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+@AutoService(ProtocolMapper.class)
+public class ExternalClaimMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
+    private static final String USER_ID_PLACEHOLDER = "**userId**";
+    private static final Logger LOGGER = Logger.getLogger(ExternalClaimMapper.class);
+    private static final String REMOTE_URL_PROPERTY = "remoteUrl";
+    private static final String JSON_PATH_EXPRESSION_PROPERTY = "jsonPath";
+    private static final String PROPAGATE_REMOTE_ERROR_PROPERTY = "propagateError";
+    static final List<ProviderConfigProperty> PROPERTIES_CONFIG;
+    static final Configuration JSON_PATH_CONFIG;
+
+    static {
+        var propertiesBuilder = ProviderConfigurationBuilder.create();
+
+        propertiesBuilder
+                .property()
+                .name(REMOTE_URL_PROPERTY)
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .label("Remote url")
+                .helpText(String.format("Remote url to get claim for the given user, use %s as placeholder", USER_ID_PLACEHOLDER))
+                .add();
+
+        propertiesBuilder
+                .property()
+                .name(JSON_PATH_EXPRESSION_PROPERTY)
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .label("Json path")
+                .helpText("Json path expression, will be applied to response")
+                .add();
+
+        propertiesBuilder
+                .property()
+                .name(PROPAGATE_REMOTE_ERROR_PROPERTY)
+                .type(ProviderConfigProperty.BOOLEAN_TYPE)
+                .label("Error propagation")
+                .defaultValue("false")
+                .helpText("If enabled will throw errors, on remote endpoint error codes and on json transformation.")
+                .add();
+
+        PROPERTIES_CONFIG = propertiesBuilder.build();
+        JSON_PATH_CONFIG = Configuration.builder()
+                .mappingProvider(new JacksonMappingProvider(JsonSerialization.mapper))
+                .jsonProvider(new JacksonJsonProvider(JsonSerialization.mapper))
+                .build();
+
+        OIDCAttributeMapperHelper.addAttributeConfig(PROPERTIES_CONFIG, ExternalClaimMapper.class);
+    }
+
+    private boolean IsEmpty(String value) {
+        return value == null || value.isBlank();
+    }
+
+    @Override
+    public String getId() {
+        return "external-claim-mapper";
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "External claim mapper";
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Mapper that requests json endpoint and adds response as access token claim with configured name";
+    }
+
+    @Override
+    public String getDisplayCategory() {
+        return TOKEN_MAPPER_CATEGORY;
+    }
+
+    @Override
+    public int getPriority() { return 100; }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return PROPERTIES_CONFIG;
+    }
+
+    public static boolean propagateError(ProtocolMapperModel model) {
+        return "true".equals(model.getConfig().get(PROPAGATE_REMOTE_ERROR_PROPERTY));
+    }
+
+    @Override
+    protected void setClaim(IDToken token, ProtocolMapperModel model, UserSessionModel user, KeycloakSession session, ClientSessionContext clientSessionCtx) {
+        var uid = user.getUser().getId();
+        var url = makeUrl(model, uid);
+        if (url == null) {
+            return;
+        }
+
+        var claimData = getClaimData(model, token, url, uid, session);
+        if (IsEmpty(claimData)) {
+            return;
+        }
+
+        var claimDataTransformed = transformResponse(model, claimData);
+        if (claimDataTransformed == null) {
+            return;
+        }
+
+        var claimName = model.getConfig().get(OIDCAttributeMapperHelper.TOKEN_CLAIM_NAME);
+        try {
+            LOGGER.infov("Setting user={0} claim={1}", uid, claimName);
+            OIDCAttributeMapperHelper.mapClaim(token, model, claimDataTransformed);
+        } catch (Exception e) {
+            LOGGER.errorv(e, "Failed to set user={0} claim={1} data={2}", uid, claimName, claimDataTransformed);
+        }
+    }
+
+    private String makeUrl(ProtocolMapperModel mappingModel, String uid) {
+        var remoteUrl = mappingModel.getConfig().get(REMOTE_URL_PROPERTY);
+        if (IsEmpty(remoteUrl)) {
+            LOGGER.warn("Remote url is required");
+            return null;
+        }
+
+        try {
+            return new URL(remoteUrl.replace(USER_ID_PLACEHOLDER, uid)).toString();
+        } catch (MalformedURLException e) {
+            LOGGER.errorv(e, "Could not create request url");
+            return null;
+        }
+    }
+
+    private String transformResponse(ProtocolMapperModel mapperModel, String data) {
+        var jsonPath = mapperModel.getConfig().get(JSON_PATH_EXPRESSION_PROPERTY);
+        if (IsEmpty(jsonPath)) {
+            return data;
+        }
+
+        try {
+            var dataObject = JsonPath.using(JSON_PATH_CONFIG).parse(data).read(jsonPath);
+            return JsonSerialization.mapper.writeValueAsString(dataObject);
+        } catch (Throwable e) {
+            LOGGER.errorv(e, "Invalid jsonPath expression");
+            if (propagateError(mapperModel)) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }
+    }
+
+    private SimpleHttp setAuth(ProtocolMapperModel model, SimpleHttp request, KeycloakSession session, IDToken token) {
+        var encodedIdToken = session.tokens().encodeAndEncrypt(token);
+        return request.auth(encodedIdToken);
+    }
+
+    private SimpleHttp setHeaders(SimpleHttp request) {
+        return request.header("Content-Type", "application/json");
+    }
+
+    private String getClaimData(ProtocolMapperModel model, IDToken token, String url, String uid, KeycloakSession session) {
+        try {
+            LOGGER.infov("Getting claim data for user={0} from url={1}", uid, url);
+            var request = SimpleHttp.doGet(url, session);
+            var response = setHeaders(setAuth(model, request, session, token)).asResponse();
+            var status = response.getStatus();
+            var success = status >= 200 && status < 400;
+            if (!success) {
+                LOGGER.warnv("Claim data request failed status={1} response={2} ", uid, response.asString(), status);
+                if (propagateError(model)) {
+                    throw new HttpResponseException(status, response.asString());
+                }
+                return null;
+            }
+
+            var claimData = response.asString();
+            LOGGER.infov("Got claim data for user={0} from url={1} length={2}", uid, url, claimData.length());
+            LOGGER.debugv("Claim data for user={0} data={2}", uid, url, claimData);
+            return claimData;
+        } catch (Exception e) {
+            LOGGER.errorv(e, "Could not get claim data for user={0} from url={1}", uid, url);
+            if (propagateError(model)) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }
+    }
+}
