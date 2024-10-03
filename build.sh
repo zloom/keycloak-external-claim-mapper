6a86183df757759ce9cc336285b8ffa046d84723
Anton Zalialdinov <zloomsky@gmail.com>
2024-10-03 23:38:22 +0200

    add tar

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mvn dependency:copy -Dartifact=com.jayway.jsonpath:json-path:2.8.0 -DoutputDirectory=build
 mvn clean package -Pconf -Ddir=../build
+tar -czvf external.claim.mapper-0.0.1.tar.gz build
