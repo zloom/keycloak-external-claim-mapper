b3089b7a1cf7f993d88cf83879ca8d05ed89f910
Anton Zalialdinov <zloomsky@gmail.com>
2024-10-04 13:12:48 +0200

    fix tar packing

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn dependency:copy -Dartifact=com.jayway.jsonpath:json-path:2.8.0 -DoutputDirectory=build
 mvn clean package -Pconf -Ddir=../build
-tar -czvf external.claim.mapper-0.0.1.tar.gz build
+tar -C build -czvf external.claim.mapper-0.0.1.tar.gz .
